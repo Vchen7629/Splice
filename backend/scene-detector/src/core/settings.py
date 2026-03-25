f85d52cf17aab454781acea9e7377d996c9b570b
Vchen7629 <chenvincent7629@gmail.com>
2026-03-25 11:56:15 -0700

    feat(scene-detector): updated settings.py with more configs

--- backend/scene-detector/src/core/settings.py
+++ backend/scene-detector/src/core/settings.py
@@ -4,16 +4,22 @@ from pydantic_settings import BaseSettings
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 ENV_FILE = PROJECT_ROOT / ".env"
 
+
 class Settings(BaseSettings):
     # general config
     LOG_LEVEL: str = "DEBUG"
+    LOG_FORMAT: str = "json"
 
     # Nats config
     NATS_URL: str = "nats://localhost:4222"
-    NATS_QUEUE_NAME: str = "scene-detetector-workers" 
-    RAW_VIDEO_SUBJECT: str = "scene-detector" # kafka topic equivalent
+    NATS_SUB_QUEUE_NAME: str = "scene-detetector-workers"
+    SCENE_SPLIT_SUBJECT: str = (
+        "jobs.video.scene-split"  # topic containing Job ID + storage path in MinIO
+    )
+    VIDEO_CHUNKS_SUBJECT: str = "jobs.video.chunks"
 
     MAX_RECONNECT_ATTEMPT: int = 5
     RECONNECT_TIME_WAIT_S: int = 2
+
 
 settings = Settings()
