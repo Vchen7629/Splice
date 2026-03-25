ae754e8cd69b6172c9e47254bb1235ff8906b401
Vchen7629 <chenvincent7629@gmail.com>
2026-03-25 12:02:55 -0700

    feat(scene-detecto): created video.py with function to split a video into chunks based on scenes using the scenedetect library

---
+++ backend/scene-detector/src/processing/video.py
@@ -0,0 +1,26 @@
+from scenedetect import detect
+from scenedetect import AdaptiveDetector
+from scenedetect import split_video_ffmpeg
+import os
+
+
+def split_into_chunks(video_path: str, output_dir: str) -> list[str]:
+    """
+    Split one video file into multiple video chunks based on scene
+    Change.
+
+    Args:
+        video_path: the location the video we are trying to split is
+        output_dir: the location the split video is saved to
+
+    Returns:
+        a list of output video dir strings
+    """
+    scene_list = detect(video_path, AdaptiveDetector())
+    split_video_ffmpeg(video_path, scene_list, output_dir)
+    video_stem = os.path.splitext(os.path.basename(video_path))[0]
+
+    return [
+        os.path.join(output_dir, f"{video_stem}-Scene-{i + 1:03d}.mp4")
+        for i in range(len(scene_list))
+    ]
