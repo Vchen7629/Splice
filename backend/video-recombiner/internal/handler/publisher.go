ca894a198d8423422a9220c97ffd2f6a162f30e1
Vchen7629 <chenvincent7629@gmail.com>
2026-03-26 20:46:33 -0700

    feat(video-recombiner): created placeholder publisher that sends nats msg that video for jobID is done processing for updating the frontend

---
+++ backend/video-recombiner/internal/handler/publisher.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"video-recombiner/internal/service"
+
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+const pubSubject = "jobs.complete"
+
+// publish a message saying the video processing is 
+// finished so frontend can update
+func PublishVideoProcessingComplete(
+	js jetstream.JetStream, 
+	msg service.VideoProcessingCompleteMessage,
+) error {
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("marshall chunk error: %w", err)
+	}
+
+	_, err = js.PublishAsync(pubSubject, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
