8fbbcbfc6a12158510075df30d4c30bcc184a451
Vchen7629 <chenvincent7629@gmail.com>
2026-04-07 16:58:21 -0700

    feat(scene-detector): added check_health.py to check if seaweedfs filer is reachable

---
+++ backend/scene-detector/src/storage/check_health.py
@@ -0,0 +1,26 @@
+from ..core.logging import logger
+from ..core.settings import settings
+import requests
+
+def check_storage_health() -> None:
+    """
+    Check if seaweedfs filer is reachable
+
+    Raises:
+        requests.ConnectionError: if seaweedfs is unreachable
+        requests.HTTPError: if seaweedfs filer returns a 5xx error
+    """
+    try:
+        response = requests.get(settings.BASE_STORAGE_URL + "/")
+        response.raise_for_status()
+    except requests.ConnectionError as e:
+        logger.error(f"seaweedfs filer unreachable", err=str(e))
+        raise
+    except requests.HTTPError as e:
+        logger.error(
+            f"seaweedfs file returned error",
+            status_code=e.response.status_code,
+            err=str(e)
+        )
+        raise
+    
