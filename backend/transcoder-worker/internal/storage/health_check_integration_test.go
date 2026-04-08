562edf40b940abbe1c0925702ca15b9fd32ac486
Vchen7629 <chenvincent7629@gmail.com>
2026-04-07 20:52:52 -0700

    tests(transcoder-worker): added integration tests for health_check.go

---
+++ backend/transcoder-worker/internal/storage/health_check_integration_test.go
@@ -0,0 +1,38 @@
+//go:build integration
+
+package storage_test
+
+import (
+	"os"
+	"testing"
+	"transcoder-worker/internal/storage"
+	"transcoder-worker/internal/test"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var sharedFilerUrl string
+
+func TestMain(m *testing.M) {
+	filerURL, cleanup := test.StartSeaweedFSFiler()
+	sharedFilerUrl = filerURL
+
+	code := m.Run()
+
+	cleanup()
+	os.Exit(code)
+}
+
+func TestCheckHealth(t *testing.T) {
+	t.Run("storage health check fails when seedweedfs is unreachable", func(t *testing.T) {
+		err := storage.CheckHealth("http://localhost:1", test.SilentLogger())
+
+		assert.Error(t, err)
+	})
+
+	t.Run("storage health check passes when seedweedfs is reachable", func(t *testing.T) {
+		err := storage.CheckHealth(sharedFilerUrl, test.SilentLogger())
+
+		assert.NoError(t, err)
+	})
+}
