c583dc6475b15454c9f8f35d24108ce27daef757
Vchen7629 <chenvincent7629@gmail.com>
2026-04-07 20:56:34 -0700

    refactor(transcoder-worker): updated transcode.go to work with seaweedfs rather than local filesystem

--- backend/transcoder-worker/internal/service/transcode.go
+++ backend/transcoder-worker/internal/service/transcode.go
@@ -12,19 +12,19 @@ import (
 // transcode by upscaling/downscaling the input video chunk to the specified value and output and return
 // the resulting video chunk. Uses lanczos algorithm is used for upscaling for now since AI super resolution
 // algo requires libtensorflow + model file and is scoped to a later phase
-func TranscodeVideo(payload VideoChunkMessage, outputDir string, logger *slog.Logger) (string, error) {
-	outDir := filepath.Join(outputDir, "jobs", payload.JobID, "transcoded")
+func TranscodeVideo(filePath, target_resolution, jobID string, logger *slog.Logger) (string, error) {
+	outDir := "/tmp/temp-processed-" + jobID
 	err := os.MkdirAll(outDir, 0755)
 	if err != nil {
 		return "", fmt.Errorf("create output dir error: %w", err)
 	}
-
-	outputPath := filepath.Join(outDir, fmt.Sprintf("chunk_%03d.mp4", payload.ChunkIndex))
-	height := strings.TrimSuffix(payload.TargetResolution, "p")
+	filename := filepath.Base(filePath)
+	outputPath := filepath.Join(outDir, filename)
+	height := strings.TrimSuffix(target_resolution, "p")
 
 	cmd := exec.Command(
 		"ffmpeg",
-		"-i", payload.StoragePath,
+		"-i", filePath,
 		"-vf", fmt.Sprintf("scale=-2:%s:flags=lanczos", height),
 		"-c:v", "libx264",
 		"-c:a", "copy",
