49222d547c6b76f9d6a5f6e7a1fbb54186ce8270
Vchen7629 <chenvincent7629@gmail.com>
2026-04-17 15:08:02 -0700

    feat(shared): added videoJobMessage type struct for use in scene-split and video-upscale nats messages

--- backend/shared/handler/nats_messages.go
+++ backend/shared/handler/nats_messages.go
@@ -1,5 +1,11 @@
 package handler
 
+type VideoJobMessage struct {
+	JobID            string `json:"job_id"`
+	TargetResolution string `json:"target_resolution"`
+	StorageURL       string `json:"storage_url"`
+}
+
 type ChunkCompleteMessage struct {
 	JobID       string `json:"job_id"`
 	ChunkIndex  int    `json:"chunk_index"`
