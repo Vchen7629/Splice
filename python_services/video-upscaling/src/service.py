ec4763cc7157adf47de42156d19b6c04b539de42
Vchen7629 <chenvincent7629@gmail.com>
2026-08-27 17:19:31 -0700

    fix: wrapped re-export in quotes

--- python_services/video-upscaling/src/service.py
+++ python_services/video-upscaling/src/service.py
@@ -7,7 +7,7 @@ from shared_handler import (
     check_js_stream_exists,
     start_health_server,
 )
-from shared_storage.check_health import check_storage_health
+from shared_storage import check_storage_health
 from .core.settings import settings
 from .processing.nats_msg import process_msg
 import asyncio
