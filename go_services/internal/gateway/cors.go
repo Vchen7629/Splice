be16dc22b66d66b114e488ad49b39727ba1a8603
Vchen7629 <chenvincent7629@gmail.com>
2026-09-02 22:31:29 -0700

    fix: added delete to cors aswell

--- go_services/internal/gateway/cors.go
+++ go_services/internal/gateway/cors.go
@@ -16,7 +16,7 @@ func Cors(next http.Handler) http.Handler {
 
 		if allowed {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		}
 
